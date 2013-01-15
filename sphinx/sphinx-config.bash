0323689971c1e8e2ba506c20eb7f28d30a2d811b
dotzero <mail@dotzero.ru>
2013-01-15 12:07:54 +0400

    First release

---
+++ sphinx/sphinx-config.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WORKDIR=`pwd`
+CONFDIR="$WORKDIR/conf.d/"
+
+LINE=$(seq -s "#" 70 | sed 's/[0-9]//g')
+
+if [ ! -d "$CONFDIR" ]; then
+  echo "# Path '$CONFDIR' is incorrect."
+  exit 1
+fi
+
+for file in "${CONFDIR%?}"/*
+do
+  if [[ -f $file ]]; then
+    FILENAME=$(basename "$file")
+
+    echo -e "${LINE}\n# Include file ${FILENAME}\n${LINE}\n"
+    cat "$file"
+  fi
+done
